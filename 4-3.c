4ac1725f50544ce1c54206aa5ae4724713ac77dc
Kakisuke1005 <n1005kjiam@gmail.com>
2019-05-11 19:24:02 +0900

    課題

---
+++ 4-3.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+int find(char *s,char c);
+int main()
+{
+  char name[]="NihonUniversity";
+  char *str;
+  int i,num;
+  num=find(name,' ');
+  switch(num){
+  case -1:
+    printf("スペース記号は見つかりませんでした\n");
+    break;
+  default:
+    printf("%d文字目に見つかりました！\n",num);
+    str=(char*)malloc(7*sizeof(char));
+    for(i=0;i<num;i++){
+      str[i]=name[i];
+    }
+    str[num]='\0';
+    printf("%s\n",str);
+    free(str);
+    break;
+  }
+  return 0;
+}
+
+int find(char *s ,char c)
+{
+  int num=0,i=0;
+  while(*s!='\0'){
+    i++;
+    if(*s==c){
+      num=i;
+      break;
+    }
+    s++;
+  }
+  if(num!=i){
+    num=-1;
+  }
+  return num;
+}
---
+++ 4-5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 15
+int get_length(char *word);
+int main()
+{
+  int num[N]={0};
+  char word[16];
+  int i=0,word_length;
+  while(1){
+    printf("%d番目の英単語を入力してください(15文字以内)：\n",i+1);
+    printf("-->  ");
+    scanf("%s",word);
+    if(word[0]=='*'){
+      break;
+    }else{
+      word_length=get_length(word);
+      num[word_length-1]++;
+    }
+    i++;
+  }
+  printf("文字数  出現回数\n");
+  for(i=0;i<N;i++){
+    printf("%d      %d\n",i+1,num[i]);
+  }
+  return 0;
+}
+
+int get_length(char *word)
+{
+	int num=0;
+	while(*word!='\0'){
+		num++;
+		word++;
+	}
+	return num;
+}
