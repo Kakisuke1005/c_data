f2637cfd0d45603c2d22aab28cea6c6b5b3beaf1
Kakisuke1005 <n1005kjiam@gmail.com>
2019-06-06 22:26:00 +0900

    変更点追加

--- 8-1.c
+++ 8-1.c
@@ -1,3 +1,22 @@
 #include<stdio.h>
+#define NUM 1000
+
+void bubble(int x[], int num);
+void swap(int *a, int *b);
+
+int n_comp=0, n_exchange=0; //比較回数と交換回数（グローバル変数）
+
 int main()
 {
+	int i,num,x[NUM];
+}
+
+void bubble(int x[],int num)
+{}
+
+void swap(int *a, int *b)
+{
+	int tmp=*a;
+	*a=*b;
+	*b=tmp;
+}
