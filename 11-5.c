57cbb2b500cd3a6b6d8594b6107a25a3ad711900
Kakisuke1005 <n1005kjiam@gmai.com>
2019-06-29 21:35:56 +0900

    未完成

--- 11-5.c
+++ 11-5.c
@@ -12,6 +12,8 @@ int InsertNode(int no,int insdata,LIST data[]);
 int DeleteNode(int no,LIST data[]);
 int getNode(int no,LIST data[]);
 
+int head=0;  //先頭の添字
+
 int main()
 {
   LIST data[MAX];
@@ -38,7 +40,7 @@ int main()
       scanf("%d",&insdata);
       id=InsertNode(no,insdata,data);
       if(id==-1){
-	printf("---挿入できませんでした---\n");
+	      printf("---挿入できませんでした---\n");
       }
       break;
     case 1:
@@ -46,7 +48,7 @@ int main()
       scanf("%d",&no);
       id=DeleteNode(no,data);
       if(id==-1){
-	printf("---削除できませんでした---\n");
+	      printf("---削除できませんでした---\n");
       }
       break;
     case 2:
@@ -54,7 +56,7 @@ int main()
       scanf("%d",&no);
       id=getNode(no,data);
       if(id==-1){
-	printf("---取得できませんでした---\n");
+      	printf("---取得できませんでした---\n");
       }
       break;
     case -1:
@@ -66,7 +68,7 @@ int main()
 
 void ShowList(LIST data[])
 {
-  int i=0;
+  int i=head;
   printf("リスト：");
   while(1){
     printf("%d ",data[i].nodedata);
@@ -78,11 +80,90 @@ void ShowList(LIST data[])
   printf("\n");
 }
 
-int InsertNode(int no,int insdata,LIST data[])
-{}
+int InsertNode(int no,int insdata,LIST data[])  //データの末尾に挿入不可　修正必須
+{
+  int blank; //空いてる場所
+  int num=0,i=head,pre=0;
+  for(blank=0;blank<MAX;blank++){ //空いてる場所を探す
+    if(data[blank].nodedata==-1){
+      break;
+    }
+  }
+  if(no<0||blank==MAX){ //挿入位置対象外と空きがない場合挿入不可
+    return -1;
+  }
+  while(1){
+    if(data[i].nodedata!=-1){
+      num++;
+      if(num==no){
+        if(no==1){
+          data[blank].nodedata=insdata;
+          data[blank].nextindex=head;
+          head=blank;
+        }else{
+          data[blank].nodedata=insdata;
+          data[blank].nextindex=data[pre].nextindex;
+          data[pre].nextindex=blank;
+        }
+        return 0;
+      }
+    }
+    pre=i;
+    i=data[i].nextindex;
+    if(data[i].nextindex==-1){
+      return -1;
+    }
+  }
+}
 
 int DeleteNode(int no,LIST data[])
-{}
+{
+  int num=0,i=head,pre=-1;
+  if(no<0){
+    return -1;
+  }
+  while(1){
+    if(data[i].nodedata!=-1){
+      num++;
+      if(no==num){
+        if(no==1){
+          head=data[i].nextindex;
+          data[i].nodedata=-1;
+          data[i].nextindex=-1;
+        }else{
+          data[pre].nextindex=data[i].nextindex;
+          data[i].nodedata=-1;
+          data[i].nextindex=-1;
+        }
+        return 0;
+      }
+    }
+    pre=i;
+    i=data[i].nextindex;
+    if(i==-1){
+      return -1;
+    }
+  }
+}
 
-int getNode(int no,LIST data[])
-{}
+int getNode(int no,LIST data[])  //本来は表示できない場所が表示される　修正
+{
+  int num=0,i=head;
+  int ans;
+  if(no<0){
+    return -1;
+  }
+  while(1){
+    if(data[i].nodedata!=-1){
+      num++;
+      if(no==num){
+        ans=data[i].nodedata;
+        return 0;
+      }
+    }
+    i=data[i].nextindex;
+    if(num==MAX){
+      return -1;
+    }
+  }
+}
