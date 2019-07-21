b01699a51177a3c200459a3e4541ba696adcd3d2
Kakisuke1005 <n1005kjiam@gmai.com>
2019-07-21 23:06:08 +0900

    未完成

---
+++ 14-5.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<string.h>
+
+#define NO_DATA -1
+#define STACK_SIZE 20
+
+int push(int data);
+int pop();
+void swap(int *a,int *b);
+void ShowStack();
+
+int stack[STACK_SIZE];
+int sp=-1; //スタックポインタ
+
+int main()
+{
+	char stack_poland[STACK_SIZE];
+	int i;
+	int num,ans;
+	int length;
+	for(i=0;i<STACK_SIZE;i++){
+		stack[i]=NO_DATA;
+	}
+	printf("逆ポーランド記法で記述された数式を入力してください\n");
+	scanf("%s",stack_poland);
+	length=strlen(stack_poland);
+	i=0;
+	while(1){
+		switch(stack_poland[i]){
+			case '+':
+				ans=pop()+pop();
+				push(ans);
+			break;
+			case '-':
+				ans=pop()-pop();
+				push(ans);
+			break;
+			case '*':
+				ans=pop()*pop();
+				push(ans);
+			break;
+			default:
+				num=stack_poland[i]-'0';
+				push(num);
+			break;
+		}
+		i++;
+		ShowStack();
+		if(i==length){
+			break;
+		}
+	}
+	printf("計算結果 = %d\n",pop());
+	return 0;
+}
+
+int push(int data)
+{
+	sp++;
+	if(sp>=STACK_SIZE){
+		sp--;
+		return-1;
+	}else{
+		stack[sp]=data;
+		return 0;
+	}
+}
+
+int pop()
+{
+	int data;
+	if(sp<0){
+		return 0;
+	}else{
+		data=stack[sp];
+		sp--;
+		return data;
+	}
+}
+
+void swap(int *a,int *b){
+	int tmp=*a;
+	*a=*b;
+	*b=tmp;
+}
+
+void ShowStack()
+{
+int i;
+printf("Stack : ");
+for ( i=0; i<=sp; i++ ){ // sp:スタックポインタ
+printf("[%d]", stack[i]);
+}
+printf(" \n");
+}
